fad34e751868cfc5b2f977530b36dd9158fe16f7
Bruno Rocha <brunorocha2674@gmail.com>
2023-07-13 09:48:15 -0300

    feat(actions): actions js docs

--- src/actions.ts
+++ src/actions.ts
@@ -1,4 +1,4 @@
-// http://www.celiomoliterno.eng.br/Arquivos/fatec/TabInferEquiv.pdf
+//http://www.celiomoliterno.eng.br/Arquivos/fatec/TabInferEquiv.pdf
 import { MissingTargetLineError } from './errors'
 import {
   andRegex,
@@ -29,6 +29,14 @@ import {
   ungroup
 } from './utils'
 
+/**
+ * Double negation elimination:
+ * @example
+ * dn(['~~p'],[0])//'p'
+ * //negation of not p is equivalent to p
+ * dn(['p'],[0])//'~~p'
+ * //p is equivalent to the negation of not p
+ */
 export function dn(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -42,6 +50,14 @@ export function dn(lines: string[], targetLines: number[]): string {
   return normalize(result)
 }
 
+/**
+ * Tautology
+ * @example
+ * ip(['p v p'],[0])//'p'
+ * //p is true is equiv. to p is true or p is true
+ * ip(['p ^ p'],[0])//'p'
+ * //p is true is equiv. to p is true and p is true
+ */
 export function ip(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -53,6 +69,14 @@ export function ip(lines: string[], targetLines: number[]): string {
   return compare(caseA, caseB) ? normalize(caseA) : invalidActionMessage
 }
 
+/**
+ * Commutation
+ * @example
+ * ip(['p ^ q'],[0])//'q ^ p'
+ * //(p and q) is equiv. to (q and p)
+ * ip(['p v q'],[0])//'q v p'
+ * //(p or q) is equiv. to (q or p)
+ */
 export function com(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -67,6 +91,14 @@ export function com(lines: string[], targetLines: number[]): string {
   return normalize(result)
 }
 
+/**
+ * Association
+ * @example
+ * ass(['p v (q v r)'],[0])//'(p v q) v r'
+ * //p or (q or r) is equiv. to (p or q) or r
+ * ass(['p ^ (q ^ r)'],[0])//'(p ^ q) ^ r'
+ * //p and (q and r) is equiv. to (p and q) and r
+ */
 export function ass(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -89,6 +121,14 @@ export function ass(lines: string[], targetLines: number[]): string {
     : normalize(group(parts[0], signal, parts[1]), signal, parts[2])
 }
 
+/**
+ * De Morgan's Theorem
+ * @example
+ * dm(['~(p ^ q)'],[0])//'~p v ~q'
+ * //The negation of (p and q) is equiv. to (not p or not q)
+ * dm(['~(p v q)'],[0])//'~p ^ ~q'
+ * //The negation of (p or q) is equiv. to (not p and not q)
+ */
 export function dm(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -105,6 +145,14 @@ export function dm(lines: string[], targetLines: number[]): string {
     : normalize(mapNot(group(target)).replace(/~{2}/g, ''))
 }
 
+/**
+ * Distribution
+ * @example
+ * dis(['p ^ (q v r)'],[0])//'(p ^ q) v (p ^ r)'
+ * //p and (q or r) is equiv. to (p and q) or (p and r)
+ * dis(['p v (q ^ r)'],[0])//'(p v q) ^ (p v r)'
+ * //p or (q and r) is equiv. to (p or q) and (p or r)
+ */
 export function dis(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -144,6 +192,11 @@ export function dis(lines: string[], targetLines: number[]): string {
   )
 }
 
+/**
+ * @example
+ * cp(['p -> q'],[0])//'~p -> ~q'
+ * //If p then q is equiv. to if not p then not q
+ */
 export function cp(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -165,6 +218,12 @@ export function cp(lines: string[], targetLines: number[]): string {
   return normalize(result)
 }
 
+/**
+ * Transposition
+ * @example
+ * cond(['p -> q'],[0])//'~q -> ~p'
+ * //If p then q is equiv. to if not q then not p
+ */
 export function cond(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -183,6 +242,12 @@ export function cond(lines: string[], targetLines: number[]): string {
   return invalidActionMessage
 }
 
+/**
+ * Material Equivalence
+ * @example
+ * bi(['p <-> q'],[0])//'(p -> q) ^ (q -> p)'
+ * //(p iff q) is equiv. to (if p is true then q is true) and (if q is true then p is true)
+ */
 export function bi(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -200,6 +265,12 @@ export function bi(lines: string[], targetLines: number[]): string {
   )
 }
 
+/**
+ * Addition
+ * @example
+ * ad(['p', 'q'],[0])//'p v q'
+ * //p is true; therefore the disjunction (p or q) is true
+ */
 export function ad(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -250,6 +321,12 @@ export function sim(lines: string[], targetLines: number[]): string {
   return invalidActionMessage
 }
 
+/**
+ * Modus Ponens
+ * @example
+ * mp(['p -> q', 'p'],[0, 1])//'q'
+ * //If p then q; p; therefore q
+ */
 export function mp(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 2) {
     throw new MissingTargetLineError(targetLines.length, 2)
@@ -267,6 +344,12 @@ export function mp(lines: string[], targetLines: number[]): string {
     : invalidActionMessage
 }
 
+/**
+ * Modus Tollens
+ * @example
+ * mt(['p -> q', '~q'],[0, 1])//'~p'
+ * //If p then q; not q; therefore not p
+ */
 export function mt(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 2) {
     throw new MissingTargetLineError(targetLines.length, 2)
@@ -286,6 +369,12 @@ export function mt(lines: string[], targetLines: number[]): string {
     : invalidActionMessage
 }
 
+/**
+ * Disjunctive Syllogism
+ * @example
+ * sd(['p v q', '~p'],[0, 1])//'q'
+ * //Either p or q, or both; not p; therefore, q
+ */
 export function sd(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 2) {
     throw new MissingTargetLineError(targetLines.length, 2)
@@ -305,6 +394,12 @@ export function sd(lines: string[], targetLines: number[]): string {
     : invalidActionMessage
 }
 
+/**
+ * Hypothetical Syllogism
+ * @example
+ * sh(['p -> q', 'q -> r'],[0, 1])//'p -> r'
+ * //If p then q; if q then r; therefore, if p then r
+ */
 export function sh(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 2) {
     throw new MissingTargetLineError(targetLines.length, 2)
@@ -327,6 +422,12 @@ export function sh(lines: string[], targetLines: number[]): string {
     : invalidActionMessage
 }
 
+/**
+ * Constructive Dilemma
+ * @example
+ * dc(['(p -> q) ^ (r -> s) ^ (p v r)'],[0])//'q v s'
+ * //If p then q; and if r then s; but p or r; therefore q or s
+ */
 export function dc(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -354,6 +455,12 @@ export function dc(lines: string[], targetLines: number[]): string {
     : invalidActionMessage
 }
 
+/**
+ * Destructive Dilemma
+ * @example
+ * dd(['(p -> q) ^ (r -> s) ^ (~q v ~s)'],[0])//'~p v ~r'
+ * //If p then q; and if r then s; but not q or not s; therefore not p or not r
+ */
 export function dd(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -382,6 +489,12 @@ export function dd(lines: string[], targetLines: number[]): string {
     : invalidActionMessage
 }
 
+/**
+ * Absorption
+ * @example
+ * abs(['p -> q'],[0])//'p -> (p ^ q)'
+ * //If p then q; therefore p then p and q
+ */
 export function abs(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 1) {
     throw new MissingTargetLineError(targetLines.length, 1)
@@ -399,6 +512,12 @@ export function abs(lines: string[], targetLines: number[]): string {
   )
 }
 
+/**
+ * Conjunction
+ * @example
+ * conj(['p', 'q'],[0, 1])//'p ^ q'
+ * //If p is true, and q is true; therefore p and q are true
+ */
 export function conj(lines: string[], targetLines: number[]): string {
   if (targetLines.length < 2) {
     throw new MissingTargetLineError(targetLines.length, 2)
